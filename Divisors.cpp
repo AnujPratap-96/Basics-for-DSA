2f49e7dd6399275d060df2bd4e6b88f3cb5a733d
Anuj Pratap Singh <160201596+AnujPratap-96@users.noreply.github.com>
2024-09-10 21:57:21 +0530

    Update Divisors.cpp

--- Divisors.cpp
+++ Divisors.cpp
@@ -1,102 +1,124 @@
 #include<bits/stdc++.h>
-          
+
 using namespace std;
 
-void print(int n)
-{
-    
-    
-    for(int i = 1; i*i<=n;i++)
-    {
-        if((n % i) == 0)
-        {
-            cout<<i<<endl;
-        if((n%i) != i)
-        {
-            cout<<n/i<<endl;
-        }
+//? Brute Force: Print all divisors of a number.
+void printDivisorsBruteForce(int n) {
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            cout << i << endl;
         }
     }
-
 }
 
-bool prime(int n){
-    int count = 0;
-    for(int i = 1; i*i<=n;i++)
-    {
-        if((n % i) == 0)
-        {
-            // cout<<i<<endl;
-            count++;
-        if((n%i) != i)
-        {
-            count++;
-            // cout<<n/i<<endl;
-        }
+//? Optimal Way: Print all divisors of a number using square root optimization.
+void printDivisorsOptimal(int n) {
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            cout << i << endl;         // Print the divisor
+            if (i != n / i) {          // If the divisor is not the square root
+                cout << n / i << endl;  // Print the corresponding divisor
+            }
         }
     }
-    if(count == 2) return true;
-    return false;
 }
 
-int allDivisors(int x)
-{
+//* Function to check if a number is prime.
+bool isPrime(int n) {
+    if (n < 2) return false;  // Numbers less than 2 are not prime
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;  // If divisible, it's not prime
+        }
+    }
+    return true;  // Otherwise, it's prime
+}
+
+//? Brute Force: Find all prime divisors of a number.
+void findPrimeDivisorsBruteForce(int x) {
     list<int> ans;
-    //* brute force
-    // for(int i = 2; i<= x; i++)
-    // {
-    //     if(x % i ==0)
-    //     {
-    //         if(prime(i)){
-    //             ans.push_back(i);
-    //         }
-    //     }
-    // }
-   
-    //? better one
-    // for(int i = 2; i*i<= x; i++)
-    // {
-    //     if(x % i ==0)
-    //     {
-    //         if(prime(i)){
-    //             ans.push_back(i);
-    //         }
-    //         if((x / i) != i)
-    //         {
-    //             if(prime(x/i)) ans.push_back(x/i);
-    //         }
-    //     }
-    // }
-
-    //! optimal one
-     for(int i = 2; i*i<= x; i++)
-    {
-        if(x % i ==0)
-        {
-            if(prime(i)){
+    for (int i = 2; i <= x; i++) {
+        if (x % i == 0) {
+            if (isPrime(i)) {
                 ans.push_back(i);
-                while(x % i == 0)
-                {
-                       x/=i;
+            }
+        }
+    }
+    
+    // Print the prime divisors
+    for (auto a : ans) {
+        cout << a << endl;
+    }
+}
+
+//? Better Way: Find prime divisors using square root optimization.
+void findPrimeDivisorsBetter(int x) {
+    list<int> ans;
+    
+    for (int i = 2; i * i <= x; i++) {
+        if (x % i == 0) {
+            if (isPrime(i)) {
+                ans.push_back(i);
+            }
+            // Check the corresponding divisor
+            if (i != x / i && isPrime(x / i)) {
+                ans.push_back(x / i);
+            }
+        }
+    }
+    
+    // Print the prime divisors
+    for (auto a : ans) {
+        cout << a << endl;
+    }
+}
+
+//! Optimal Way: Find prime divisors using trial division.
+void findPrimeDivisorsOptimal(int x) {
+    list<int> ans;
+    
+    // Check for prime factors from 2 to sqrt(x)
+    for (int i = 2; i * i <= x; i++) {
+        if (x % i == 0) {
+            if (isPrime(i)) {
+                ans.push_back(i);
+                while (x % i == 0) {
+                    x /= i;  // Remove all factors of i
                 }
             }
         }
     }
-
-    if(x != 1) ans.push_back(x);
     
-    for(auto a : ans)
-    {
-        cout<<a<<endl;
+    // If x is still greater than 1, it must be prime
+    if (x > 1) {
+        ans.push_back(x);
+    }
+
+    // Print the prime divisors
+    for (auto a : ans) {
+        cout << a << endl;
     }
 }
-        
-int main(){
+
+int main() {
     int m;
-    cout<<"Enter a number ";
-    cin>>m;
-    // print(m);
-    // cout<<"The given number "<<m<<" is prime : "<<prime(m);
-    allDivisors(m);
+    cout << "Enter a number: ";
+    cin >> m;
+
+    // Uncomment to use the brute force method to print all divisors
+    // printDivisorsBruteForce(m);
+
+    // Uncomment to use the optimal method to print all divisors
+    // printDivisorsOptimal(m);
+
+    // Brute Force: Find prime divisors
+    // findPrimeDivisorsBruteForce(m);
+
+    // Better: Find prime divisors
+    // findPrimeDivisorsBetter(m);
+
+    // Optimal: Find prime divisors
+    findPrimeDivisorsOptimal(m);
+
     return 0;
 }
