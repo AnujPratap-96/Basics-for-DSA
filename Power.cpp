fa2a4aa6128ff34af48c4c6ba7bcb629bd994197
Anuj Pratap Singh <160201596+AnujPratap-96@users.noreply.github.com>
2024-09-11 08:49:33 +0530

    Update Power.cpp

--- Power.cpp
+++ Power.cpp
@@ -1,27 +1,32 @@
 #include<bits/stdc++.h>
-          
+
 using namespace std;
 
-double power(double x , int n)
-{   int pow = abs(n);
-    double ans = 1;
-    while(pow>0)
-    {
-         if(pow%2 == 1)
-         {
-            ans *=x;
-            pow--; 
-         }
-         else{
-            pow=pow/2;
-            x *=x;
-         }
+//? Function to calculate x raised to the power n (x^n) using exponentiation by squaring
+double power(double x, int n) {
+    int pow = abs(n);  // Take the absolute value of n to handle negative exponents
+    double ans = 1;    // Initialize result
+    
+    // Exponentiation by squaring
+    while (pow > 0) {
+        if (pow % 2 == 1) {      // If pow is odd, multiply the result by x
+            ans *= x;
+            pow--;               // Decrement pow
+        } else {
+            pow /= 2;            // Halve pow
+            x *= x;              // Square x
+        }
     }
-    if(n<0) return 1.0/ans;
+    
+    // If the exponent is negative, return the reciprocal
+    if (n < 0) {
+        return 1.0 / ans;
+    }
     return ans;
 }
-        
-int main(){
-    cout<<power(5,5);
+
+int main() {
+    // Example usage
+    cout << power(5, 5) << endl;  // Should print 3125
     return 0;
 }
