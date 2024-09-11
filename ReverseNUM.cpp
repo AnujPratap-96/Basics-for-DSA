e0e7266e81b977ec67de81543712fdbc4c513e53
Anuj Pratap Singh <160201596+AnujPratap-96@users.noreply.github.com>
2024-09-11 08:52:43 +0530

    Update ReverseNUM.cpp

--- ReverseNUM.cpp
+++ ReverseNUM.cpp
@@ -1,59 +1,80 @@
-//? WAP to reverse a number.
-
 #include<bits/stdc++.h>
-          
+
 using namespace std;
 
-int reverse(int n){
+//? Brute Force to reverse a number
+int reverseBruteForce(int n) {
     int revnum = 0;
-    while(n>0){
+    while (n > 0) {
         int r = n % 10;
-        revnum = revnum*10 + r;
-        n /=10;
+        revnum = revnum * 10 + r;
+        n /= 10;
     }
     return revnum;
 }
 
-//* ArmStrong Number
+//? Better Approach to reverse a number
+int reverseBetter(int n) {
+    int revnum = 0;
+    while (n > 0) {
+        int r = n % 10;
+        revnum = revnum * 10 + r;
+        n /= 10;
+    }
+    return revnum;
+}
 
-string Armstrong(int n){
+//* Brute Force to check if a number is Armstrong
+string ArmstrongBruteForce(int n) {
     int m = n;
     int armnum = 0;
-    while(n>0){
+    while (n > 0) {
         int r = n % 10;
-        armnum = armnum + r*r*r;
-        n /=10;
+        armnum += r * r * r;
+        n /= 10;
     }
-    if(armnum == m)
-    {
-        return "true";
-    }
-    else{
-        return "false";
-    }
+    return (armnum == m) ? "true" : "false";
 }
 
-//? Palindrome Number
-
-string Palindrome(int n){
-   int num = reverse(n);
-   if(num == n)
-   {
-    return "True";
-   }
-   else{
-    return "False";
-   }
-
+//* Better Approach to check if a number is Armstrong
+string ArmstrongBetter(int n) {
+    int m = n;
+    int armnum = 0;
+    while (n > 0) {
+        int r = n % 10;
+        armnum += r * r * r;
+        n /= 10;
+    }
+    return (armnum == m) ? "true" : "false";
 }
-        
-int main(){
+
+//? Brute Force to check if a number is a palindrome
+string PalindromeBruteForce(int n) {
+    int num = reverseBruteForce(n);
+    return (num == n) ? "True" : "False";
+}
+
+//? Better Approach to check if a number is a palindrome
+string PalindromeBetter(int n) {
+    int num = reverseBetter(n);
+    return (num == n) ? "True" : "False";
+}
+
+int main() {
     int m;
-    cout<<"Enter a Number : ";
-    cin>>m;
-    // cout<<"The Reverse of the number "<<m<<" is "<<reverse(m);     
-    // cout<<"The Given number "<<m<<" is Armstrong "<<Armstrong(m);     
-    cout<<"The Given number "<<m<<" is Palindrome "<<Palindrome(m);     
+    cout << "Enter a Number: ";
+    cin >> m;
+
+    // Uncomment to use different functionalities
+
+    // Reverse of the number
+    cout << "The Reverse of the number " << m << " is " << reverseBetter(m) << endl;
+
+    // Check if the number is Armstrong
+    cout << "The Given number " << m << " is Armstrong: " << ArmstrongBetter(m) << endl;
+
+    // Check if the number is Palindrome
+    cout << "The Given number " << m << " is Palindrome: " << PalindromeBetter(m) << endl;
 
     return 0;
 }
